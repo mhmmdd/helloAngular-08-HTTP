968c4213cce01227bcd91733087205fc53a6ea02
mhmmdd <mhmmd.dd@gmail.com>
2016-07-03 19:08:51 +0300

    Fixes #1

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,12 +1,39 @@
 import {Component} from 'angular2/core';
-import {ROUTER_DIRECTIVES} from "angular2/router";
+import {HttpService} from "./http.service";
 
 @Component({
   selector: 'my-app',
   template: `
+    <div>
+      <div class="input">
+        <label for="title">Title</label>
+        <input type="text" id="title" #title />
+      </div>
+      <div class="input">
+        <label for="body">Body</label>
+        <input type="text" id="body" #body />
+      </div>
+      <div class="input">
+        <label for="user-id">User ID</label>
+        <input type="text" id="user-id" #userId />
+      </div>
+      <button (click)="onPost(title.value, body.value, userId.value)">Post Data</button>
+      <button (click)="onGetPosts()">Get all Posts</button>
+      <p>Response: {{response | json}}</p>
+    </div>
   `,
-  directives: [ROUTER_DIRECTIVES]
+  providers: [HttpService]
 })
 export class AppComponent {
+  response: string;
 
+  constructor(private _httpService: HttpService) {}
+
+  onGetPosts() {
+    this._httpService.getPosts()
+      .subscribe(
+        response => this.response = response,
+        error => console.log(error)
+      )
+  }
 }
---
+++ dev/http.service.ts
@@ -0,0 +1,14 @@
+import {Injectable} from "angular2/core";
+import {Http} from "angular2/http";
+import {Observable} from "rxjs/Observable";
+import 'rxjs/Rx';
+
+@Injectable()
+export class HttpService {
+  constructor(private _http: Http) {}
+
+  getPosts():Observable<any> {
+    return this._http.get('http://jsonplaceholder.typicode.com/posts')
+      .map(res => res.json());
+  }
+}
